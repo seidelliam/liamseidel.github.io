aa4c0bafb22f3913dbaf28fbdef2964cfd76cc24
seidelliam <seidelliam@gmail.com>
2024-08-02 14:44:39 -0700

    Header load

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ function splitScroll(){
     const controller = new ScrollMagic.Controller();
 
     new ScrollMagic.Scene({
-        duration: '180%',
+        duration: '200%',
         triggerElement: '.navbar',
         triggerHook: 0
     })
     .addIndicators()
     .setPin('.navbar')
     .addTo(controller);
+    data-scrollmagic-pin-spacer.setAttribute('width', '11vw');
 }
 
 function projectsScroll(){
@@ -18,7 +19,7 @@ function projectsScroll(){
     new ScrollMagic.Scene({
         duration: '80%',
         triggerElement: '.projects-title',
-        triggerHook: 0
+        triggerHook: 0.05
     })
     .addIndicators()
     .setPin('.projects-title')
